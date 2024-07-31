8e1faff46fbc6051e95df5cc2670cb6d62fd268d
Jimmbo19 <jega190112@gmail.com>
2024-07-31 03:10:18 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function(input) {
+  let who = ["el perro", "Mi abuela", "el cartero", " el pajaro"];
+  let action = ["comio", "rompio", "robo", "vendio"];
+  let what = ["mi tarea", "mi telefono", " el carro"];
+  let when = [
+    "antes de clases",
+    " mientras dormia",
+    "mientras me bañaba",
+    "en mi descanso",
+    "despues del trabajo"
+  ];
+
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+
+  document.querySelector("#excusa").innerHTML = excuse;
 };
